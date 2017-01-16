17a0c8d72b35c92c37c42a411189e5bbf5bb5954
Jan Lavička <ja.lavicka@gmail.com>
2017-01-16 22:24:40 +0100

    Added files and assets macros for generating links

---
+++ src/Latte/Macros.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of the Voonne platform (http://www.voonne.org)
+ *
+ * Copyright (c) 2016 Jan Lavička (mail@janlavicka.name)
+ *
+ * For the full copyright and license information, please view the file licence.md that was distributed with this source code.
+ */
+
+namespace Voonne\Storage\Latte;
+
+use Latte\Compiler;
+use Latte\MacroNode;
+use Latte\Macros\MacroSet;
+use Latte\PhpWriter;
+use Voonne\Storage\InvalidArgumentException;
+
+
+class Macros extends MacroSet
+{
+
+	public static function install(Compiler $compiler)
+	{
+		$macros = new static($compiler);
+
+		$macros->addMacro('assetLink', [$macros, 'macroAssetLink']);
+
+		$macros->addMacro('fileLink', [$macros, 'macroFileLink']);
+
+		return $macros;
+	}
+
+
+	public function macroAssetLink(MacroNode $macroNode, PhpWriter $phpWriter)
+	{
+		if (!isset($macroNode->args) || empty($macroNode->args)) {
+			throw new InvalidArgumentException('Invalid asset link: name must be non-empty string.');
+		}
+
+		return 'echo $this->global->uiPresenter->link(":Admin:Api:Assets:default", ["name" => "' . trim($macroNode->args) . '"]);';
+	}
+
+
+	public function macroFileLink(MacroNode $macroNode, PhpWriter $phpWriter)
+	{
+		$args = explode(',', $macroNode->args);
+
+		if (!isset($args[0]) || empty($args[0])) {
+			throw new InvalidArgumentException('Invalid asset link: directory name must be non-empty string.');
+		}
+
+		if (!isset($args[1]) || empty($args[1])) {
+			throw new InvalidArgumentException('Invalid asset link: file name must be non-empty string.');
+		}
+
+		return 'echo $this->global->uiPresenter->link(":Admin:Api:Files:default", ["directoryName" => "' . trim($args[0]) . '", "fileName" => "' . trim($args[1]) . '"]);';
+	}
+
+}
---
+++ tests/unit/Latte/MacrosTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Voonne\TestStorage\Latte;
+
+use Codeception\Test\Unit;
+use Latte\Compiler;
+use Latte\MacroNode;
+use Latte\PhpWriter;
+use Mockery;
+use Mockery\MockInterface;
+use UnitTester;
+use Voonne\Storage\InvalidArgumentException;
+use Voonne\Storage\Latte\Macros;
+
+
+class MacroTest extends Unit
+{
+
+	/**
+	 * @var UnitTester
+	 */
+	protected $tester;
+
+	/**
+	 * @var MockInterface
+	 */
+	private $compiler;
+
+	/**
+	 * @var Macros
+	 */
+	private $macros;
+
+
+	protected function _before()
+	{
+		$this->compiler = Mockery::mock(Compiler::class);
+
+		$this->macros = new Macros($this->compiler);
+	}
+
+
+	protected function _after()
+	{
+		Mockery::close();
+	}
+
+
+	public function testMacroAssetLink()
+	{
+		$macroNode = Mockery::mock(MacroNode::class);
+		$phpWriter = Mockery::mock(PhpWriter::class);
+
+		$macroNode->args = 'styles/admin.css';
+
+		$this->assertEquals(
+			'echo $this->global->uiPresenter->link(":Admin:Api:Assets:default", ["name" => "styles/admin.css"]);',
+			$this->macros->macroAssetLink($macroNode, $phpWriter));
+	}
+
+
+	public function testMacroAssetLinkMissingParameter()
+	{
+		$macroNode = Mockery::mock(MacroNode::class);
+		$phpWriter = Mockery::mock(PhpWriter::class);
+
+		$macroNode->args = '';
+
+		$this->expectException(InvalidArgumentException::class);
+		$this->macros->macroAssetLink($macroNode, $phpWriter);
+	}
+
+
+	public function testMacroFileLink()
+	{
+		$macroNode = Mockery::mock(MacroNode::class);
+		$phpWriter = Mockery::mock(PhpWriter::class);
+
+		$macroNode->args = 'photos, photo.png';
+
+		$this->assertEquals(
+			'echo $this->global->uiPresenter->link(":Admin:Api:Files:default", ["directoryName" => "photos", "fileName" => "photo.png"]);',
+			$this->macros->macroFileLink($macroNode, $phpWriter));
+	}
+
+
+	public function testMacroFileLinkMissingParameter()
+	{
+		$macroNode = Mockery::mock(MacroNode::class);
+		$phpWriter = Mockery::mock(PhpWriter::class);
+
+		$macroNode->args = 'photos';
+
+		$this->expectException(InvalidArgumentException::class);
+		$this->macros->macroFileLink($macroNode, $phpWriter);
+
+		$macroNode->args = '';
+
+		$this->expectException(InvalidArgumentException::class);
+		$this->macros->macroFileLink($macroNode, $phpWriter);
+	}
+
+}
